7b784de887ff1be00eeea87c6e7272b928a1f976
DayNice <76981513+DayNice@users.noreply.github.com>
2026-02-13 23:51:01 +0900

    Add statistics utils

--- src/aflux/utils/__init__.py
+++ src/aflux/utils/__init__.py
@@ -4,10 +4,16 @@ from ._batch_utils import (
     iter_batch,
     run_batch,
 )
+from ._stats_utils import (
+    compute_sample_indices,
+    compute_sample_size,
+)
 
 __all__ = [
     "aiter_batch",
     "arun_batch",
+    "compute_sample_indices",
+    "compute_sample_size",
     "iter_batch",
     "run_batch",
 ]
