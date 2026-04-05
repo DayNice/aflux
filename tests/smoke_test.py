e830ede569ccacc1d64617333c11a5e1da70e534
DayNice <76981513+DayNice@users.noreply.github.com>
2026-04-05 14:26:37 +0900

    chore(test): update smoke test

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,7 +1,17 @@
 # ruff: disable[F401]
 import aflux
+import aflux.cli
+import aflux.cli.ros
+import aflux.cli.video
 import aflux.protocols
+import aflux.protocols.bucket
 import aflux.types
+import aflux.types.bucket
+import aflux.types.ros
+import aflux.types.video
 import aflux.utils
+import aflux.utils.bucket
+import aflux.utils.dataframe
 import aflux.utils.ros
+import aflux.utils.video
 # ruff: enable[F401]
